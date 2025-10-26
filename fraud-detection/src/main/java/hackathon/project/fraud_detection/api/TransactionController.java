79e5d3084dde9d909a431fad002d54b98b5d6883
abdukarimovakm <yuukiasuna0@bk.ru>
2025-10-26 17:17:47 +0300

    add export to csv

--- fraud-detection/src/main/java/hackathon/project/fraud_detection/api/TransactionController.java
+++ fraud-detection/src/main/java/hackathon/project/fraud_detection/api/TransactionController.java
@@ -3,17 +3,18 @@ package hackathon.project.fraud_detection.api;
 import hackathon.project.fraud_detection.api.dto.request.ChangeStatusRequest;
 import hackathon.project.fraud_detection.api.dto.request.TransactionRequest;
 import hackathon.project.fraud_detection.api.dto.response.TransactionResponse;
-import hackathon.project.fraud_detection.exceptions.DBWritingException;
+import hackathon.project.fraud_detection.rules.service.ExporterService;
 import hackathon.project.fraud_detection.rules.service.TransactionProcessingService;
 import hackathon.project.fraud_detection.storage.entity.TransactionEntity;
-import hackathon.project.fraud_detection.storage.entity.TransactionStatus;
 import hackathon.project.fraud_detection.storage.repository.TransactionRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +29,7 @@ public class TransactionController {
 
     private final TransactionProcessingService transactionProcessingService;
     private final TransactionRepository transactionRepository;
+    private final ExporterService exporterService;
 
     @PostMapping("/transactions")
     public ResponseEntity<?> processTransaction(@Valid @RequestBody TransactionRequest transactionRequest) {
@@ -61,6 +63,23 @@ public class TransactionController {
                     "Invalid status: " + changeStatusRequest.status()));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/transactions/export")
+    public ResponseEntity<?> exportAllTransactionsToCsv(
+            @AuthenticationPrincipal UserDetails userDetails,
+            HttpServletResponse response) {
+
+        log.info("Exporting all transactions to by user: {}", userDetails.getUsername());
+        List<TransactionEntity> transactions = transactionRepository.findAll();
+
+        try {
+            exporterService.exportToCsv(transactions, response, userDetails);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                    .body(TransactionResponse.error(e.getCause().getMessage()));
         }
     }
 }
---
+++ fraud-detection/src/main/java/hackathon/project/fraud_detection/rules/service/ExporterService.java
@@ -0,0 +1,78 @@
+package hackathon.project.fraud_detection.rules.service;
+
+import hackathon.project.fraud_detection.storage.entity.TransactionEntity;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@Component
+public class ExporterService {
+
+    public void exportToCsv(List<TransactionEntity> transactions, HttpServletResponse response, UserDetails userDetails) throws IOException {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+        String filename = "transactions_" + timestamp + ".csv";
+
+        response.setContentType("text/csv");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filename + "\"");
+        response.setHeader("X-Exported-By", userDetails.getUsername());
+        response.setHeader("X-Export-Timestamp", LocalDateTime.now().toString());
+        response.setHeader("X-Total-Records", String.valueOf(transactions.size()));
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write("UUID;CorrelationId;Id транзакции;Дата;Сумма;Тип транзакции;Категория;Отправитель;Получатель;Статус;Подозрительная;IP-адрес;Локация;Устройство;Сработавшие правила;Дата обработки\n");
+
+            for (TransactionEntity transaction : transactions) {
+                writer.write(convertToCsvRow(transaction) + "\n");
+            }
+            log.info("Exported {} transactions to CSV", transactions.size());
+        } catch (Exception e) {
+            log.error("Error exporting transactions to CSV", e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Error exporting transactions");
+        }
+    }
+
+    public String convertToCsvRow(TransactionEntity transaction) {
+        return String.join(";",
+                escapeCsvField(transaction.getId().toString()),
+                escapeCsvField(transaction.getCorrelationId()),
+                escapeCsvField(transaction.getOriginalTransactionId()),
+                escapeCsvField(transaction.getTimestamp() != null ?
+                        transaction.getTimestamp().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : ""),
+                escapeCsvField(transaction.getAmount() != null ?
+                        transaction.getAmount().toString() : ""),
+                escapeCsvField(transaction.getTransactionType()),
+                escapeCsvField(transaction.getMerchantCategory()),
+                escapeCsvField(transaction.getSenderAccount()),
+                escapeCsvField(transaction.getReceiverAccount()),
+                escapeCsvField(transaction.getStatus().toString()),
+                escapeCsvField(String.valueOf(transaction.getSuspicious())),
+                escapeCsvField(transaction.getIpAddress()),
+                escapeCsvField(transaction.getLocation()),
+                escapeCsvField(transaction.getDeviceUsed()),
+                escapeCsvField(transaction.getTriggeredRules()),
+                escapeCsvField(transaction.getProcessedAt() != null ?
+                        transaction.getProcessedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : "")
+        );
+    }
+
+    public String escapeCsvField(String field) {
+        if (field == null) {
+            return "";
+        }
+        if (field.contains(";") || field.contains("\"") || field.contains("\n")) {
+            return "\"" + field.replace("\"", "\"\"") + "\"";
+        }
+        return field;
+    }
+}
